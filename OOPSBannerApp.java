89d00c5e76405f7a6c510722ef2c8231a1c791ce
veera-raghavan-officiall <veera.veeraraghavanofficial@gmail.com>
2026-02-19 10:12:07 +0530

    "git: ASCII Art"

---
+++ OOPSBannerApp.java
@@ -0,0 +1,25 @@
+/**
+ * OOPSBannerApp UC2 - Banner Display Application
+ * * This class displays a stylized "OOPS" banner in the console.
+ * The spacing is carefully calibrated to match the visual layout 
+ * of the provided reference image.
+ * * @author Developer
+ * @version 2.1
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        
+        // Line 1: Top caps of O, O, P and S
+        System.out.println("   ****    " + " " + "   ****    " + " " + "**********  "+ " " + "**********");
+        System.out.println(" **     ** " + " " + " **     ** " + " " + "*********** "+ " " + "**********");
+        System.out.println("**       **" + " " + "**       **" + " " + "**        **"+ " " + "**        ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**        **"+ " " + "**        ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       ** "+ " " + "**********");
+        System.out.println("**       **" + " " + "**       **" + " " + "*********   "+ " " + "**********");
+        System.out.println("**       **" + " " + "**       **" + " " + "***         "+ " " + "        **");
+        System.out.println(" **     ** " + " " + " **     ** " + " " + "***         "+ " " + "        **");
+        System.out.println("   ****    " + " " + "   ****    " + " " + "***         "+ " " + "**********");
+
+    }
+}
