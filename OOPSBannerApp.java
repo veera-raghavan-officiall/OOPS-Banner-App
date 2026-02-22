8df1452ab7b43f1f3ee90b3fdf15c7c179f314ee
veera-raghavan-officiall <veera.veeraraghavanofficial@gmail.com>
2026-02-22 15:12:31 +0530

    "git : UC4 Update"

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,30 @@
 /**
- * OOPSBannerApp UC2 - Banner Display Application
- *
- * This class displays a stylized "OOPS" banner in the console.
- * The spacing is carefully calibrated to match the visual layout
- * of the provided reference image.
- *
- * @author Developer
- * @version 2.1
+ * OOPSBannerApp UC4 – OOPS Banner Application (Use Case 4)
+ * * This class extends UC3 by storing banner lines in a String array and 
+ * using a loop to print them, improving modularity and reusability.
+ * * @author Developer
+ * @version 4.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+        
+        // 1. Create a String array to store all lines of the banner
+        String[] bannerLines = {
+            String.join(" ","   ****    ","   ****    ","**********  ","**********"),
+            String.join(" "," **     ** "," **     ** ","*********** ","**********"),
+            String.join(" ","**       **","**       **","**        **","**        "),
+            String.join(" ","**       **","**       **","**        **","**        "),
+            String.join(" ","**       **","**       **","**       ** ","**********"),
+            String.join(" ","**       **","**       **","**********  ","**********"),
+            String.join(" ","**       **","**       **","***         ","        **"),
+            String.join(" "," **     ** "," **     ** ","***         ","        **"),
+            String.join(" ","   ****    ","   ****    ","***         ","**********"),
+        };
 
-        // Line 1: Top caps of O, O, P and S
-        System.out.println("   ****    " + " " + "   ****    " + " " + "**********  "+ " " + "**********");
-        System.out.println(" **     ** " + " " + " **     ** " + " " + "*********** "+ " " + "**********");
-        System.out.println("**       **" + " " + "**       **" + " " + "**        **"+ " " + "**        ");
-        System.out.println("**       **" + " " + "**       **" + " " + "**        **"+ " " + "**        ");
-        System.out.println("**       **" + " " + "**       **" + " " + "**       ** "+ " " + "**********");
-        System.out.println("**       **" + " " + "**       **" + " " + "*********   "+ " " + "**********");
-        System.out.println("**       **" + " " + "**       **" + " " + "***         "+ " " + "        **");
-        System.out.println(" **     ** " + " " + " **     ** " + " " + "***         "+ " " + "        **");
-        System.out.println("   ****    " + " " + "   ****    " + " " + "***         "+ " " + "**********");
+        // 2. Use a for-each loop to print each element sequentially
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
