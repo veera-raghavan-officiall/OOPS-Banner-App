9f38c01e03a0bcc522f25508c4bf42c77a8f5e25
veera-raghavan-officiall <veera.veeraraghavanofficial@gmail.com>
2026-02-22 15:21:58 +0530

    "git: UC4 code update"

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,16 @@
 /**
  * OOPSBannerApp UC4 – OOPS Banner Application (Use Case 4)
- * * This class extends UC3 by storing banner lines in a String array and 
+ *
+ * This class extends UC3 by storing banner lines in a String array and
  * using a loop to print them, improving modularity and reusability.
- * * @author Developer
+ *
+ * @author Developer
  * @version 4.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        
+
         // 1. Create a String array to store all lines of the banner
         String[] bannerLines = {
             String.join(" ","   ****    ","   ****    ","**********  ","**********"),
